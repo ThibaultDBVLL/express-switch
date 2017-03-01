4ca1239a05e1ca78c85395fbd17a68428a871e44
thibault <thibault.daboville@gmail.com>
2017-03-01 10:39:45 +0100

    début difficile

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 // write you're code here Good Luck
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(function (req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('you posted:\n');
+  res.end(JSON.stringify(req.body, null, 2));
+})
+;
